9d988fe9d4ad7748207ba3c3c9bc211038575a05
[Sara Sterck] <[sasterck14@gmail.com]>
2022-11-05 23:37:38 -0600

    adding to index js

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const Engineer = require('./lib/Engineer');
 const Employee = require('./lib/Employee');
 const validator = require('email-validator');
 
+let teamString = '';
+let teamArray = [];
+
+console.clear();
+console.log('Team Info/Portfolio by Sara')
+
 // now let's see how to generate the card aka main function of the app
 async function main() {
     try {
         await prompt()
+        
         for(let i = 0; i <teamArray.length; i++) {
             teamString = teamString + html.generateCard(teamArray[i]);
         }
+        let finalHtml = html.generateHTML(teamString)
+        
+        console.clear();
+        console.log('generate index.html file')
+
     }
 }
