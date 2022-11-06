25d343509605c4e3f720ea7bdf4b3199db5ddae5
[Sara Sterck] <[sasterck14@gmail.com]>
2022-11-05 23:04:17 -0600

    adding async function

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const inquirer = require ('inquirer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
 const validator = require('email-validator');
+
+// now let's see how to generate the card aka main function of the app
+async function main() {
+    try {
+        await prompt()
+        for(let i = 0; i <teamArray.length; i++) {
+            teamString = teamString + html.generateCard(teamArray[i]);
+        }
+    }
+}
