dfa5e1c878112a0dc01bca9e6d5f1e175b1859e8
[Sara Sterck] <[sasterck14@gmail.com]>
2022-11-05 22:50:33 -0600

    adding to index js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const inquirer = require ('inguirer');
+// Making const requirements
+const inquirer = require ('inquirer');
 const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const validator = require('email-validator');
